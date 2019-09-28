c79490a2e6443a9bfb0882abbf2fdb71d3f90372
zjj <2019292338@qq.com>
2019-09-28 19:42:53 +0800

    jsonp封装完成

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,4 +1,17 @@
 import jsonp from 'common/js/jsonp'
+<<<<<<< HEAD
 export default function getRecommend() {
     const url = "https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg"
+=======
+import { jsonpCommonParam, options, UrlRecommend } from './config'
+
+// 获取推荐页面的数据
+export function getRecommend() {
+  let data = Object.assign({}, jsonpCommonParam, {
+      uin: 0,
+      platform: "h5",
+      needNewCode: 1
+  });
+  return jsonp(UrlRecommend, data, options);
+>>>>>>> jsonp封装完成
 }
--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -2,9 +2,28 @@
   <div>搜索</div>
 </template>
 <script>
+<<<<<<< HEAD:src/components/search/search.vue
 export default {
   name: 'search'
 }
+=======
+
+  import { getRecommend } from 'api/recommend'
+
+  export default {
+    created() {
+      console.log(123);
+      this._getRecommend();
+    },
+    methods: {
+      _getRecommend() {
+        getRecommend().then((res) => {
+          console.log(res);
+        });
+      }
+    }
+  }
+>>>>>>> jsonp封装完成:src/components/hello.vue
 </script>
 <style scoped>
 </style>
